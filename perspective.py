4ec08bfb791c701203c682f215fad42947131b2f
Oleksii Stremetskyi <stremetskyi.o@gmail.com>
2019-03-01 16:48:22 +0200

    Refactored perspective module to PerspectiveTransform class.

--- perspective.py
+++ perspective.py
@@ -1,31 +1,33 @@
 import cv2
 import numpy as np
 
-_x_start = 267
-_x_end = 1040
-_y_end_src = 670
-_y_start_src = 450
-_y_start_dst = 0
-_y_end_dst = 720
-_src = np.float32([[_x_start, _y_end_src], [_x_end, _y_end_src], [687, _y_start_src], [594, _y_start_src]])
-_dst = np.float32([[_x_start, _y_end_dst], [_x_end, _y_end_dst], [_x_end, _y_start_dst], [_x_start, _y_start_dst]])
 
+class PerspectiveTransform:
+    _y_start_src = 450
+    _y_end_src = 670
+    _default_src = ((267, _y_end_src), (1040, _y_end_src), (687, _y_start_src), (594, _y_start_src))
+    _default_shape = (720, 1280)
 
-def get_perspective_transform():
-    return cv2.getPerspectiveTransform(_src, _dst)
+    def __init__(self, src=_default_src, shape=_default_shape):
+        """
+        :param src : 4 points in counter-clockwise order, starting from bottom-left
+        :param shape : shape of the image being transformed
+        """
+        self.src = np.float32(src)
+        self.dst = np.float32([[self.src[0, 0], shape[0]],
+                               [self.src[1, 0], shape[0]],
+                               [self.src[1, 0], 0],
+                               [self.src[0, 0], 0]])
+        self.m = cv2.getPerspectiveTransform(self.src, self.dst)
 
+    def draw_src(self, img):
+        cv2.polylines(img, [self.src.astype(np.int32)], True, (0, 255, 0), 2)
 
-def draw_src(img):
-    cv2.polylines(img, [_src.astype(np.int32)], True, (0, 255, 0), 2)
+    def draw_dst(self, img):
+        cv2.polylines(img, [self.dst.astype(np.int32)], True, (0, 255, 0), 2)
 
+    def warp(self, img):
+        return cv2.warpPerspective(img, self.m, img.shape[1::-1], flags=cv2.INTER_LINEAR)
 
-def draw_dst(img):
-    cv2.polylines(img, [_dst.astype(np.int32)], True, (0, 255, 0), 2)
-
-
-def warp(img, m):
-    return cv2.warpPerspective(img, m, img.shape[1::-1], flags=cv2.INTER_LINEAR)
-
-
-def unwarp(img, m):
-    return cv2.warpPerspective(img, m, img.shape[1::-1], flags=cv2.INTER_LINEAR | cv2.WARP_INVERSE_MAP)
+    def unwarp(self, img):
+        return cv2.warpPerspective(img, self.m, img.shape[1::-1], flags=cv2.INTER_LINEAR | cv2.WARP_INVERSE_MAP)
