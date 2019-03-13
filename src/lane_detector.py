fd412274109531caefe862afd688162ca1d480c3
Oleksii Stremetskyi <stremetskyi.o@gmail.com>
2019-03-13 23:32:50 +0200

    Average line center starts using previous frames.

--- src/lane_detector.py
+++ src/lane_detector.py
@@ -1,4 +1,5 @@
 from collections import deque
+from operator import itemgetter, attrgetter
 
 import cv2
 import numpy as np
@@ -20,6 +21,20 @@ class FrameData:
     def __init__(self, line_centers, line_fits):
         self.line_centers = line_centers
         self.line_fits = line_fits
+
+    @staticmethod
+    def monotonic(x):
+        dx = np.diff(x)
+        return np.all(dx <= 0) or np.all(dx >= 0)
+
+    @staticmethod
+    def avg_next_line_center(old):
+        if len(old) == 0:
+            return None
+        if len(old) > 1 and FrameData.monotonic(old):
+            linear_fit = np.poly1d(np.polyfit(np.arange(len(old)), old, 1))
+            return int(linear_fit(len(old)))
+        return sum(old) // len(old)
 
 
 class LaneDetector:
@@ -56,22 +71,39 @@ class LaneDetector:
         lines_img = np.zeros_like(r_x)
         lines_img[(r_x == 1) | (s_x == 1)] = 1
 
-        # Calculate histogram to find start of the lanes at bottom of the image
-        hist = np.sum(lines_img[lines_img.shape[0] * 2 // 3:], axis=0)
-        # Multiplication and clipping allows to correctly detect dashed lines
-        hist = np.clip(hist * 3, 0, np.max(hist))
-
         y = np.arange(img.shape[0], -1, -self.window_height)
+        window = np.ones(self.window_width)
 
-        line_centers = self.find_centers(hist, lines_img, y)
-        if line_centers is not None:
+        line_centers_ref = None
+
+        if len(self.frames) > 0:
+            hist = np.sum(lines_img[y[1]:y[0]], axis=0)
+            line_centers_avg = [
+                self.find_next_center(hist, window, FrameData.avg_next_line_center(self.get_prev_line_centers(i)))
+                for i in range(2)]
+            if all(e is not None for e in line_centers_avg):
+                line_centers_ref = line_centers_avg
+
+        if line_centers_ref is None:
+            # Calculate histogram to find start of the lanes at bottom of the image
+            hist = np.sum(lines_img[lines_img.shape[0] * 2 // 3:], axis=0)
+            # Multiplication and clipping allows to correctly detect dashed lines
+            hist = np.clip(hist * 3, 0, np.max(hist))
+            # Find initial left and right lines pixels using convolution
+            convolve_signal = np.convolve(hist, window)
+            peaks = signal.find_peaks(convolve_signal,
+                                      distance=lane_width_p - self.window_width)[0] - self.window_width2
+            if len(peaks) == 2:
+                line_centers_ref = peaks
+
+        if line_centers_ref is not None:
+            line_centers = self.find_centers(lines_img, line_centers_ref, y, window)
             lines_img, line_fits = self.find_features(lines_img, line_centers, y)
             self.frames.append(FrameData(line_centers, line_fits))
 
             if self.overlay_lanes:
                 lines_img = self.perspective.unwarp(lines_img)
                 img = cv2.addWeighted(img, 0.8, lines_img, 1, 0)
-                self.draw_hist(img, hist)
                 self.annotate_frame(img, line_fits)
                 return img
 
@@ -84,17 +116,8 @@ class LaneDetector:
             else:
                 return lines_img * 255
 
-    def find_centers(self, hist, lines_img, y):
-        window = np.ones(self.window_width)
-
-        # Find initial left and right lines pixels using convolution
-        convolve_signal = np.convolve(hist, window)
-        peaks = signal.find_peaks(convolve_signal, distance=lane_width_p - self.window_width)[0] - self.window_width2
-        if len(peaks) != 2:
-            # TODO: Detect missing line(s) using previous frames
-            return None
-        line_centers = [[peaks[i]] for i in range(len(peaks))]
-        # TODO: Average line centers using previous frames
+    def find_centers(self, lines_img, line_centers_ref, y, window):
+        line_centers = [[line_centers_ref[i]] for i in range(len(line_centers_ref))]
         for i in range(1, len(y) - 1):
             bm = y[i]
             tp = y[i + 1]
@@ -147,12 +170,6 @@ class LaneDetector:
             x_r = int(line_fits[1](y))
             img[y, x_l:x_r] = 90
 
-    def draw_hist(self, img, hist):
-        hist = img.shape[0] - self.hist_height / np.max(hist) * hist
-        cv2.rectangle(img, (0, img.shape[0] - self.hist_height), img.shape[1::-1], (0, 0, 0), cv2.FILLED)
-        hist_fig = np.dstack((np.linspace(0, img.shape[1], img.shape[1]), hist))
-        cv2.polylines(img, [hist_fig.astype(np.int32)], False, (255, 255, 255), thickness=2)
-
     def annotate_frame(self, img, line_fits):
         vehicle_x = (line_fits[0](img.shape[0]) + line_fits[1](img.shape[0])) / 2
         center_distance = (img.shape[1] / 2 - vehicle_x) * self.m2p_ratio_x
@@ -175,3 +192,6 @@ class LaneDetector:
             r = (1 + (2 * a * y + b) ** 2) ** 1.5 / np.abs(2 * a)
             radius.append(r)
         return sum(radius) / len(radius)
+
+    def get_prev_line_centers(self, idx):
+        return list(map(itemgetter(0), map(itemgetter(idx), map(attrgetter('line_centers'), self.frames))))
