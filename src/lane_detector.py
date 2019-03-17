58d2a2a32311ec00004aef737efb30f49c576b52
Oleksii Stremetskyi <stremetskyi.o@gmail.com>
2019-03-17 16:48:39 +0200

    Refactoring lane detector.

--- src/lane_detector.py
+++ src/lane_detector.py
@@ -22,38 +22,16 @@ class FrameData:
         self.line_centers = line_centers
         self.line_fits = line_fits
 
-    @staticmethod
-    def monotonic(x):
-        dx = np.diff(x)
-        return np.all(dx <= 0) or np.all(dx >= 0)
-
-    @staticmethod
-    def avg_next_line_center(old):
-        if len(old) == 0:
-            return None
-        if len(old) > 1 and FrameData.monotonic(old):
-            linear_fit = np.poly1d(np.polyfit(np.arange(len(old)), old, 1))
-            return int(linear_fit(len(old)))
-        return sum(old) // len(old)
-
-    @staticmethod
-    def avg_next_line_fit(old, new):
-        if len(old) == 0:
-            return new
-        return np.average(np.row_stack((old, new)), axis=0)
-
 
 class LaneDetector:
 
-    def __init__(self, window_width=100, window_height=80, draw_convolution_windows=False, hist_height=50,
-                 overlay_lanes=True, history_size=5):
+    def __init__(self, window_width=100, window_height=80, history_size=5, draw_mode='lane', overlay_result=True):
         self.window_width = window_width
         self.window_width2 = window_width // 2
         self.window_width34 = window_width * 3 // 4
         self.window_height = window_height
-        self.draw_convolution_windows = draw_convolution_windows & (not overlay_lanes)
-        self.hist_height = hist_height
-        self.overlay_lanes = overlay_lanes
+        self.draw_mode = draw_mode
+        self.overlay_result = overlay_result
 
         self.frames = deque(maxlen=history_size)
         self.calibration_params = calibration_params.load()
@@ -63,68 +41,57 @@ class LaneDetector:
         self.m2p_ratio_y = lane_dash_length_m / lane_dash_length_p
 
     def process_image(self, img):
-        # Prepare image
-        img = cv2.undistort(img, *self.calibration_params)
+        img = self.undistort(img)
+        lines_img = self.binary_gradient(img)
+        lines_img = self.warp_perspective(lines_img)
 
-        # Separate channels
-        rgb_r = colorspace.rgb_channels(img)[0]
-        hsl_s = colorspace.hsl_channels(img)[1]
+        line_centers = self.find_lane_line_centers(lines_img, history=self.frames)
 
-        # Calculate and combine gradients
-        r_x = gradient.abs_x(rgb_r, 9, (40, 170))
-        s_x = gradient.abs_x(hsl_s, 9, (25, 220))
-        lines_img = np.zeros_like(r_x)
-        lines_img[(r_x == 1) | (s_x == 1)] = 1
-
-        # Warp image to aerial view
-        lines_img = self.perspective.warp(lines_img)
-
-        y = np.arange(img.shape[0], -1, -self.window_height)
-        window = np.ones(self.window_width)
-
-        line_centers_ref = None
-
-        if len(self.frames) > 0:
-            hist = np.sum(lines_img[y[1]:y[0]], axis=0)
-            line_centers_avg = [
-                self.find_next_center(hist, window, FrameData.avg_next_line_center(self.get_prev_line_centers(i)))
-                for i in range(2)]
-            if all(e is not None for e in line_centers_avg):
-                line_centers_ref = line_centers_avg
-
-        if line_centers_ref is None:
-            # Calculate histogram to find start of the lanes at bottom of the image
-            hist = np.sum(lines_img[lines_img.shape[0] * 2 // 3:], axis=0)
-            # Multiplication and clipping allows to correctly detect dashed lines
-            hist = np.clip(hist * 3, 0, np.max(hist))
-            # Find initial left and right lines pixels using convolution
-            convolve_signal = np.convolve(hist, window)
-            peaks = signal.find_peaks(convolve_signal,
-                                      distance=lane_width_p - self.window_width)[0] - self.window_width2
-            if len(peaks) == 2:
-                line_centers_ref = peaks
-
-        if line_centers_ref is not None:
-            line_centers = self.find_centers(lines_img, line_centers_ref, y, window)
-            lines_img, line_fits = self.find_features(lines_img, line_centers, y)
+        if line_centers is not None:
+            lines_img, line_fits = self.highlight_lane_features(lines_img, line_centers, mode=self.draw_mode,
+                                                                history=self.frames)
             self.frames.append(FrameData(line_centers, line_fits))
 
-            if self.overlay_lanes:
-                lines_img = self.perspective.unwarp(lines_img)
-                img = cv2.addWeighted(img, 0.8, lines_img, 1, 0)
-                self.annotate_frame(img, line_fits)
-                return img
+            if self.overlay_result:
+                unwarped = self.unwarp_overlay(img, lines_img)
+                self.annotate(unwarped, line_fits)
+                return unwarped
 
             return lines_img
         else:
             if len(self.frames) > 0:
                 self.frames.popleft()
-            if self.overlay_lanes:
+            if self.overlay_result:
                 return img
             else:
                 return lines_img * 255
 
-    def find_centers(self, lines_img, line_centers_ref, y, window):
+    def undistort(self, img):
+        return cv2.undistort(img, *self.calibration_params)
+
+    @staticmethod
+    def binary_gradient(img):
+        # Separate channels
+        rgb_r = colorspace.rgb_channels(img)[0]
+        hsl_s = colorspace.hsl_channels(img)[1]
+        # Calculate and combine gradients
+        r_x = gradient.abs_x(rgb_r, 9, (40, 170))
+        s_x = gradient.abs_x(hsl_s, 9, (25, 220))
+        lines_img = np.zeros_like(r_x)
+        lines_img[(r_x == 1) | (s_x == 1)] = 1
+        return lines_img
+
+    def warp_perspective(self, lines_img):
+        return self.perspective.warp(lines_img)
+
+    def find_lane_line_centers(self, lines_img, history=None):
+        window = np.ones(self.window_width)
+        y = np.arange(lines_img.shape[0], -1, -self.window_height)
+
+        line_centers_ref = self._calc_line_centers_ref(lines_img, window, y, history)
+        if line_centers_ref is None:
+            return None
+
         line_centers = [[line_centers_ref[i]] for i in range(len(line_centers_ref))]
         for i in range(1, len(y) - 1):
             bm = y[i]
@@ -132,24 +99,13 @@ class LaneDetector:
             slice_hist = np.sum(lines_img[tp:bm], axis=0)
             for j in range(len(line_centers)):
                 if len(line_centers[j]) == i:
-                    center = self.find_next_center(slice_hist, window, line_centers[j][-1])
+                    center = self._find_next_center(slice_hist, window, line_centers[j][-1])
                     if center is not None:
                         line_centers[j].append(center)
         return line_centers
 
-    def find_next_center(self, hist, window, center):
-        line_min = max(center - self.window_width34, 0)
-        line_max = min(center + self.window_width34, len(hist))
-        if line_max - line_min >= self.window_width2 // 2:
-            # TODO: need to handle off the screen lines
-            convolution_signal = np.convolve(hist[line_min:line_max], window, mode='same')
-            if np.count_nonzero(convolution_signal) > 0:
-                peaks = signal.find_peaks(convolution_signal)[0]
-                if len(peaks) == 1:
-                    return line_min + peaks[0]
-        return None
-
-    def find_features(self, img, line_centers, y):
+    def highlight_lane_features(self, img, line_centers, mode='lane', history=None):
+        y = np.arange(img.shape[0], -1, -self.window_height)
         img_r, img_g, img_b = np.zeros_like(img), np.zeros_like(img), np.zeros_like(img)
 
         for i in range(len(y) - 1):
@@ -160,41 +116,90 @@ class LaneDetector:
                     lt = max(centers[i] - self.window_width2, 0)
                     rt = min(centers[i] + self.window_width2, img.shape[1])
                     img_ch[tp:bm, lt:rt] = img[tp:bm, lt:rt] * 255
-                    if self.draw_convolution_windows:
+                    if mode == 'conv':
                         cv2.rectangle(img_g, (lt, bm), (rt, tp), 255, thickness=2)
 
-        line_fits = [
-            np.poly1d(FrameData.avg_next_line_fit(self.get_prev_line_fits(i),
-                                                  np.polyfit(y[:len(line_centers[i])], line_centers[i], 2)))
-            for i in range(len(line_centers))]
+        line_fits = [np.poly1d(self._avg_line_fit(i, np.polyfit(y[:len(line_centers[i])], line_centers[i], 2), history))
+                     for i in range(len(line_centers))]
 
-        if not self.draw_convolution_windows:
-            self.draw_lane(img_g, line_fits)
+        res = np.dstack((img_r, img_g, img_b))
 
-        return np.dstack((img_r, img_g, img_b)), line_fits
+        if mode == 'lane':
+            self._draw_lane(res, line_fits)
+        elif mode == 'fit':
+            self._draw_lines(res, line_fits)
 
-    @staticmethod
-    def draw_lane(img, line_fits):
-        for y in range(0, img.shape[0]):
-            x_l = int(line_fits[0](y))
-            x_r = int(line_fits[1](y))
-            img[y, x_l:x_r] = 90
+        return res, line_fits
 
-    def annotate_frame(self, img, line_fits):
-        vehicle_x = (line_fits[0](img.shape[0]) + line_fits[1](img.shape[0])) / 2
-        center_distance = (img.shape[1] / 2 - vehicle_x) * self.m2p_ratio_x
+    def unwarp_overlay(self, img, lines_img):
+        lines_img = self.perspective.unwarp(lines_img)
+        img = cv2.addWeighted(img, 0.8, lines_img, 1, 0)
+        return img
+
+    def annotate(self, img, line_fits):
+        center_distance = self._lane_position(img, line_fits)
         if center_distance == 0:
             text1 = 'Vehicle is centered on the lane'
         else:
             side = 'left' if center_distance < 0 else 'right'
             text1 = 'Vehicle is %.1f m. %s of center' % (abs(center_distance), side)
-        text2 = 'Curve radius is %.2f m.' % self.curve_radius(img.shape[0], line_fits)
+        text2 = 'Curve radius is %.2f m.' % self._curve_radius(img.shape[0], line_fits)
         cv2.putText(img, text1, (10, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (200, 200, 200),
                     thickness=2, lineType=cv2.LINE_AA)
         cv2.putText(img, text2, (10, 110), cv2.FONT_HERSHEY_DUPLEX, 2, (200, 200, 200),
                     thickness=2, lineType=cv2.LINE_AA)
 
-    def curve_radius(self, y, line_fits):
+    @staticmethod
+    def _draw_lane(img, line_fits):
+        for y in range(0, img.shape[0]):
+            x_l = int(line_fits[0](y))
+            x_r = int(line_fits[1](y))
+            img[y, x_l:x_r, 1] = 90
+
+    @staticmethod
+    def _draw_lines(img, line_fits):
+        y = np.arange(img.shape[0])
+        for fit in line_fits:
+            x = fit(y)
+            pts = np.column_stack((x, y)).astype(np.int32)
+            cv2.polylines(img, [pts], False, (0, 255, 0), thickness=3)
+
+    def _calc_line_centers_ref(self, lines_img, window, y, history):
+        if history is not None and len(history) > 0:
+            hist = np.sum(lines_img[y[1]:y[0]], axis=0)
+            line_centers_avg = [self._find_next_center(hist, window, self._avg_line_center(i, history))
+                                for i in range(2)]
+            if all(e is not None for e in line_centers_avg):
+                return line_centers_avg
+
+        # Calculate histogram to find start of the lanes at bottom of the image
+        hist = np.sum(lines_img[lines_img.shape[0] * 2 // 3:], axis=0)
+        # Multiplication and clipping allows to correctly detect dashed lines
+        hist = np.clip(hist * 3, 0, np.max(hist))
+        # Find initial left and right lines pixels using convolution
+        convolve_signal = np.convolve(hist, window)
+        peaks = signal.find_peaks(convolve_signal,
+                                  distance=lane_width_p - self.window_width)[0] - self.window_width2
+        if len(peaks) == 2:
+            return peaks
+        return None
+
+    def _find_next_center(self, hist, window, center):
+        line_min = max(center - self.window_width34, 0)
+        line_max = min(center + self.window_width34, len(hist))
+        if line_max - line_min >= self.window_width2 // 2:
+            convolution_signal = np.convolve(hist[line_min:line_max], window, mode='same')
+            if np.count_nonzero(convolution_signal) > 0:
+                peaks = signal.find_peaks(convolution_signal)[0]
+                if len(peaks) == 1:
+                    return line_min + peaks[0]
+        return None
+
+    def _lane_position(self, img, line_fits):
+        vehicle_x = (line_fits[0](img.shape[0]) + line_fits[1](img.shape[0])) / 2
+        return (img.shape[1] / 2 - vehicle_x) * self.m2p_ratio_x
+
+    def _curve_radius(self, y, line_fits):
         radius = []
         for fit in line_fits:
             a = fit.c[0] * self.m2p_ratio_x / self.m2p_ratio_y ** 2
@@ -203,8 +208,27 @@ class LaneDetector:
             radius.append(r)
         return sum(radius) / len(radius)
 
-    def get_prev_line_centers(self, idx):
-        return list(map(itemgetter(0), map(itemgetter(idx), map(attrgetter('line_centers'), self.frames))))
+    @staticmethod
+    def monotonic(x):
+        dx = np.diff(x)
+        return np.all(dx <= 0) or np.all(dx >= 0)
 
-    def get_prev_line_fits(self, idx):
-        return list(map(attrgetter('c'), map(itemgetter(idx), map(attrgetter('line_fits'), self.frames))))
+    def _avg_line_center(self, idx, history):
+        if history is None:
+            return None
+        old = list(map(itemgetter(0), map(itemgetter(idx), map(attrgetter('line_centers'), history))))
+        if len(old) == 0:
+            return None
+        if len(old) > 1 and self.monotonic(old):
+            linear_fit = np.poly1d(np.polyfit(np.arange(len(old)), old, 1))
+            return int(linear_fit(len(old)))
+        return sum(old) // len(old)
+
+    @staticmethod
+    def _avg_line_fit(idx, new, history):
+        if history is None:
+            return new
+        old = list(map(attrgetter('c'), map(itemgetter(idx), map(attrgetter('line_fits'), history))))
+        if len(old) == 0:
+            return new
+        return np.average(np.row_stack((old, new)), axis=0)
